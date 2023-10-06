01d2f4d03c0e971bcffec738493d25edd0df0b44
apps-auth <business.appsauth@gmail.com>
2023-10-06 13:18:02 -0300

    6.0.6

--- lib/generated/intl/messages_pt.dart
+++ lib/generated/intl/messages_pt.dart
@@ -19,20 +19,36 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'pt';
 
-  final Map<String, dynamic> messages = _notInlinedMessages(_notInlinedMessages);
-  static _notInlinedMessages(_) => <String, Function> {
-    "access_to_location_denied" : MessageLookupByLibrary.simpleMessage("Acesso ao local negado"),
-    "access_to_location_permanently_denied" : MessageLookupByLibrary.simpleMessage("Acesso ao local negado permanentemente"),
-    "allow_access_to_the_location_services" : MessageLookupByLibrary.simpleMessage("Permitir acesso aos serviços de localização."),
-    "allow_access_to_the_location_services_from_settings" : MessageLookupByLibrary.simpleMessage("Permita o acesso aos serviços de localização para este aplicativo usando as configurações do dispositivo."),
-    "cant_get_current_location" : MessageLookupByLibrary.simpleMessage("Não é possível obter a localização atual"),
-    "finding_place" : MessageLookupByLibrary.simpleMessage("Finding place..."),
-    "no_result_found" : MessageLookupByLibrary.simpleMessage("No result found"),
-    "ok" : MessageLookupByLibrary.simpleMessage("Ok"),
-    "please_check_your_connection" : MessageLookupByLibrary.simpleMessage("Por favor, verifique sua conexão"),
-    "please_make_sure_you_enable_gps_and_try_again" : MessageLookupByLibrary.simpleMessage("Certifique-se de ativar o GPS e tente novamente"),
-    "search_place" : MessageLookupByLibrary.simpleMessage("Pesquisar endereço"),
-    "server_error" : MessageLookupByLibrary.simpleMessage("Erro de servidor"),
-    "unnamedPlace" : MessageLookupByLibrary.simpleMessage("Lugar sem nome")
-  };
+  final Map<String, dynamic> messages =
+      _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function>{
+        "access_to_location_denied":
+            MessageLookupByLibrary.simpleMessage("Acesso ao local negado"),
+        "access_to_location_permanently_denied":
+            MessageLookupByLibrary.simpleMessage(
+                "Acesso ao local negado permanentemente"),
+        "allow_access_to_the_location_services":
+            MessageLookupByLibrary.simpleMessage(
+                "Permitir acesso aos serviços de localização."),
+        "allow_access_to_the_location_services_from_settings":
+            MessageLookupByLibrary.simpleMessage(
+                "Permita o acesso aos serviços de localização para este aplicativo usando as configurações do dispositivo."),
+        "cant_get_current_location": MessageLookupByLibrary.simpleMessage(
+            "Não é possível obter a localização atual"),
+        "finding_place":
+            MessageLookupByLibrary.simpleMessage("Encontrando lugar..."),
+        "no_result_found":
+            MessageLookupByLibrary.simpleMessage("Nenhum resultado encontrado"),
+        "ok": MessageLookupByLibrary.simpleMessage("Ok"),
+        "please_check_your_connection": MessageLookupByLibrary.simpleMessage(
+            "Por favor, verifique sua conexão"),
+        "please_make_sure_you_enable_gps_and_try_again":
+            MessageLookupByLibrary.simpleMessage(
+                "Certifique-se de ativar o GPS e tente novamente"),
+        "search_place":
+            MessageLookupByLibrary.simpleMessage("Pesquisar endereço"),
+        "server_error":
+            MessageLookupByLibrary.simpleMessage("Erro de servidor"),
+        "unnamedPlace": MessageLookupByLibrary.simpleMessage("Lugar sem nome")
+      };
 }
