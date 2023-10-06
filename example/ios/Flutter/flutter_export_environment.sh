e78b82ac83d787572f70fbcf5f16f5af95dbfcf7
apps-auth <business.appsauth@gmail.com>
2023-10-06 12:59:44 -0300

    fix translations

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\ALTFORCE\OneDrive\Documentos\Altforce\codes\modern_form_google_map_location_picker\example"
+export "FLUTTER_APPLICATION_PATH=C:\Users\ALTFORCE\OneDrive\Documentos\packages\modern_form_google_map_location_picker\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
