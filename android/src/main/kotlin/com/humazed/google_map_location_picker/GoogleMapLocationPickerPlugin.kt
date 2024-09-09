96e5967093d28c7ca45f34e277a9037e878b1daf
xavieraltforce <alan.xavier@altforce.com.br>
2024-09-09 15:43:19 -0300

    6.2.7

--- android/src/main/kotlin/com/humazed/google_map_location_picker/GoogleMapLocationPickerPlugin.kt
+++ android/src/main/kotlin/com/humazed/google_map_location_picker/GoogleMapLocationPickerPlugin.kt
@@ -31,7 +31,7 @@ class GoogleMapLocationPickerPlugin(act: Activity?) : MethodCallHandler {
         }
         if (call.method == "getSigningCertSha1") {
             try {
-                val info: PackageInfo = activity.packageManager.getPackageInfo(call.arguments<String>(), PackageManager.GET_SIGNATURES)
+                val info: PackageInfo = activity.packageManager.getPackageInfo(call.arguments<String?>(), PackageManager.GET_SIGNATURES)
                 for (signature in info.signatures) {
                     val md: MessageDigest = MessageDigest.getInstance("SHA1")
                     md.update(signature.toByteArray())
