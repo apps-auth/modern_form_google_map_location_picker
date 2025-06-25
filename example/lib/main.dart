4233c209b7844d37ba6c049bc6215281f12d720e
jfperon <joao.perondini@altforce.com.br>
2025-06-25 15:41:29 -0300

    7.0.0

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:google_map_location_picker/generated/l10n.dart'
+import 'package:modern_form_google_map_location_picker/generated/l10n.dart'
     as location_picker;
-import 'package:google_map_location_picker/google_map_location_picker.dart';
+import 'package:modern_form_google_map_location_picker/google_map_location_picker.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'generated/i18n.dart';
@@ -20,7 +20,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-//      theme: ThemeData.dark(),
+      //      theme: ThemeData.dark(),
       title: 'location picker',
       localizationsDelegates: const [
         location_picker.S.delegate,
@@ -39,40 +39,40 @@ class _MyAppState extends State<MyApp> {
         Locale('ru', ''),
       ],
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('location picker'),
-        ),
-        body: Builder(builder: (context) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () async {
-                    LocationResult? result = await showLocationPicker(
-                      context,
-                      "API_KEY",
-                      initialCenter: LatLng(31.1975844, 29.9598339),
-//                      automaticallyAnimateToCurrentLocation: true,
-//                      mapStylePath: 'assets/mapStyle.json',
-                      myLocationButtonEnabled: true,
-                      // requiredGPS: true,
-                      layersButtonEnabled: true,
-                      // countries: ['AE', 'NG']
+        appBar: AppBar(title: const Text('location picker')),
+        body: Builder(
+          builder: (context) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () async {
+                      LocationResult? result = await showLocationPicker(
+                        context,
+                        "API_KEY",
+                        initialCenter: LatLng(31.1975844, 29.9598339),
+                        //                      automaticallyAnimateToCurrentLocation: true,
+                        //                      mapStylePath: 'assets/mapStyle.json',
+                        myLocationButtonEnabled: true,
+                        // requiredGPS: true,
+                        layersButtonEnabled: true,
+                        // countries: ['AE', 'NG']
 
-//                      resultCardAlignment: Alignment.bottomCenter,
-                      desiredAccuracy: LocationAccuracy.best,
-                    );
-                    debugPrint("result = $result");
-                    setState(() => _pickedLocation = result);
-                  },
-                  child: Text('Pick location'),
-                ),
-                Text(_pickedLocation.toString()),
-              ],
-            ),
-          );
-        }),
+                        //                      resultCardAlignment: Alignment.bottomCenter,
+                        desiredAccuracy: LocationAccuracy.best,
+                      );
+                      debugPrint("result = $result");
+                      setState(() => _pickedLocation = result);
+                    },
+                    child: Text('Pick location'),
+                  ),
+                  Text(_pickedLocation.toString()),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
