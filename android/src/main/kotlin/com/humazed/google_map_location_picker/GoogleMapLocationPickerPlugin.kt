626a780925a6d5f65c3cb2a1a365e1093a76eef5
Adriel <business.appsauth@gmail.com>
2025-08-01 15:05:11 -0300

    fix android native

--- android/src/main/kotlin/com/humazed/google_map_location_picker/GoogleMapLocationPickerPlugin.kt
+++ android/src/main/kotlin/com/humazed/google_map_location_picker/GoogleMapLocationPickerPlugin.kt
@@ -1,9 +1,14 @@
 package com.humazed.google_map_location_picker
 
 import android.app.Activity
+import android.content.Context
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
+import androidx.annotation.NonNull
 import androidx.annotation.UiThread
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -12,14 +17,46 @@ import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.math.BigInteger
 import java.security.MessageDigest
 
-class GoogleMapLocationPickerPlugin(act: Activity?) : MethodCallHandler {
-    var activity: Activity = act!!
+class GoogleMapLocationPickerPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
+    private lateinit var channel: MethodChannel
+    private var activity: Activity? = null
+    private var context: Context? = null
 
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "google_map_location_picker")
+        channel.setMethodCallHandler(this)
+        context = flutterPluginBinding.applicationContext
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        activity = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivity() {
+        activity = null
+    }
+
+    // Manter compatibilidade com a API antiga
     companion object {
         @JvmStatic
         fun registerWith(registrar: Registrar) {
             val channel = MethodChannel(registrar.messenger(), "google_map_location_picker")
-            channel.setMethodCallHandler(GoogleMapLocationPickerPlugin(registrar.activity()))
+            val plugin = GoogleMapLocationPickerPlugin()
+            plugin.activity = registrar.activity()
+            plugin.context = registrar.context()
+            channel.setMethodCallHandler(plugin)
         }
     }
 
@@ -32,8 +69,14 @@ class GoogleMapLocationPickerPlugin(act: Activity?) : MethodCallHandler {
             "getSigningCertSha1" -> {
                 val packageName = call.argument<String>("packageName")
                 if (packageName != null) {
+                    val currentActivity = activity
+                    if (currentActivity == null) {
+                        result.error("NO_ACTIVITY", "Activity not available", null)
+                        return
+                    }
+                    
                     try {
-                        val info: PackageInfo = activity.packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+                        val info: PackageInfo = currentActivity.packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
                         for (signature in info.signatures) {
                             val md: MessageDigest = MessageDigest.getInstance("SHA1")
                             md.update(signature.toByteArray())
@@ -43,9 +86,11 @@ class GoogleMapLocationPickerPlugin(act: Activity?) : MethodCallHandler {
                             val hex: String = String.format("%0${bytes.size shl 1}x", bigInteger)
 
                             result.success(hex)
+                            return
                         }
                     } catch (e: Exception) {
                         result.error("ERROR", e.toString(), null)
+                        return
                     }
                 } else {
                     result.error("INVALID_ARGUMENT", "Package name must not be null", null)
